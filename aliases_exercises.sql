995b7e0085a31b8e79c3da1e97d2bb4d127bdd55
ksbleek <kyle.s.bleeker@gmail.com>
2023-11-29 16:43:53 -0600

    added DOB to birthdate

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,5 @@
 USE employees;
 
-SELECT CONCAT(emp_no, ' - ', last_name, ' ', first_name) AS full_name, birth_date
+SELECT CONCAT(emp_no, ' - ', last_name, ' ', first_name) AS full_name, birth_date AS DOB
 from employees
 limit 10;
